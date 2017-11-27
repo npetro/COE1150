270377b469c7daafdc83336bdeaa06e564f0de5a
Nick Petro <nickpetro@pitt.edu>
2017-11-27 00:44:35 -0500

    Minor tweaks

--- Lab2/server.c
+++ Lab2/server.c
@@ -41,10 +41,13 @@ int main(int argc, char const *argv[])
 	    }
 	    valread = read(new_socket , buffer, 1024);
 	    printf("%s\n",buffer);
-		//call functions here
-		strcat(holdBuffer,"Content-Length:13\r\nConnection: keep-alive\r\n");
-		strcat(holdBuffer,buffer);
-		strcat(holdBuffer, "\r\n\r\n");
+		if (tolower(buffer[0]) == 'd') {
+			holdBuffer = decode(buffer);
+		} else if (tolower(buffer[0]) == 'e') {
+			holdBuffer = encode(buffer);
+		} else {
+			printf("Invalid Message\n");
+		}
 	    send(new_socket , holdBuffer , strlen(holdBuffer) , 0 );
 	    printf("Message sent\n");
 	    //return 0;
@@ -53,8 +56,9 @@ int main(int argc, char const *argv[])
 	}
 }
 
-char* encode(char *modBuffer) {
-	for (int i = 0; i < strlen(modBuffer); i = i + 1 ) {
+int encode(int bufferPtr) {
+	char *modBuffer = bufferPtr;
+	for (int i = 2; i < strlen(modBuffer); i = i + 1 ) {
 		if (tolower(modBuffer[i]) != 'z'){
 			modBuffer[i] = tolower(modBuffer[i]) + 1;
 		} else if (tolower(modBuffer[i]) == 'z') {
@@ -66,8 +70,9 @@ char* encode(char *modBuffer) {
 	return modBuffer;
 }
 
-char* decode(char *modBuffer) {
-	for (int i = 0; i < strlen(modBuffer); i = i + 1 ) {
+int decode(int bufferPtr) {
+	char *modBuffer = bufferPtr;
+	for (int i = 2; i < strlen(modBuffer); i = i + 1 ) {
 		if (modBuffer[i] == 'A'){
 			modBuffer[i] = 'z';
 		} else if (tolower(modBuffer[i]) != 'a') {
